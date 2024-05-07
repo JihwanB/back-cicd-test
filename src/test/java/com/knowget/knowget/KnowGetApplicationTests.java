703a8a5e46f98fc5bcc14a1b973888e58f7dd75d
JihwanB <leyqorwlghks@gmail.com>
2024-05-08 06:55:05 +0900

    Initial Commit

---
+++ src/test/java/com/knowget/knowget/KnowGetApplicationTests.java
@@ -0,0 +1,37 @@
+package com.knowget.knowget;
+
+// @SpringBootTest
+class KnowGetApplicationTests {
+
+	// @Autowired
+	// private UserRepository userRepository;
+	//
+	// @Autowired
+	// private PostRepository postRepository;
+	//
+	// @Test
+	// void timeAutoCreationTest() throws Exception {
+	// 	User user = User.builder()
+	// 		.id("test")
+	// 		.name("test")
+	// 		.password("test")
+	// 		.phone("test")
+	// 		.email("test@test.com")
+	// 		.build();
+	// 	userRepository.save(user);
+	//
+	// 	Post post = Post.builder()
+	// 		.title("test")
+	// 		.content("test")
+	// 		.user(user)
+	// 		.type("test")
+	// 		.build();
+	// 	Post result = postRepository.save(post);
+	//
+	// 	System.out.println(
+	// 		"createTime = " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(result.getWrittenTime()));
+	// 	System.out.println(
+	// 		"modifiedTime = " + DateTimeFormatter.ofPattern("yyyy-MM-ss HH:mm:ss").format(result.getModifiedTime()));
+	// }
+
+}
